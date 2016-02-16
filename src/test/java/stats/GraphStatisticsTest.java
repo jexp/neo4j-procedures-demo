3f0bd16078c0353495028891ce505b84180c2c29
Michael Hunger <github@jexp.de>
2016-02-16 23:46:05 +0100

    embedded testing works now

--- src/test/java/stats/GraphStatisticsTest.java
+++ src/test/java/stats/GraphStatisticsTest.java
@@ -7,15 +7,14 @@ import org.neo4j.procedure.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.neo4j.test.TestGraphDatabaseFactory;
+
+import org.neo4j.kernel.GraphDatabaseAPI;
 import org.neo4j.kernel.impl.proc.Procedures;
-import org.neo4j.logging.Log;
 
 public class GraphStatisticsTest {
-	@BeforeClass public static void registerProcedures() throws Exception {
-	    new Procedures().register(GraphStatistics.class);
-	}
-    @Test public void testDegree() {
+    @Test public void testDegree() throws Exception {
 	    GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+	    ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(GraphStatistics.class);
         // given Alice knowing Bob and Charlie and Dan knowing no-one
         db.execute("CREATE (alice:User)-[:KNOWS]->(bob:User),(alice)-[:KNOWS]->(charlie:User),(dan:User)").close();
 
@@ -26,9 +25,9 @@ public class GraphStatisticsTest {
         assertTrue(res.hasNext());
         Map<String,Object> row = res.next();
         assertEquals("User", row.get("label"));
-        assertEquals(0, row.get("min"));
-        assertEquals(2, row.get("max"));
-        assertEquals(4, row.get("count"));
+        assertEquals(0L, row.get("min"));
+        assertEquals(2L, row.get("max"));
+        assertEquals(4L, row.get("count"));
         assertFalse(res.hasNext());
     }
 }
